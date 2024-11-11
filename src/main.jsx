5bd66c39b21a598f37708f40efd2fa4f570ba928
sumon-ray <raysumon220020@gmail.com>
2024-11-11 21:27:05 +0600

    update setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { router } from "./Routes/Routes.jsx";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+<App /> 
   </React.StrictMode>
 );
